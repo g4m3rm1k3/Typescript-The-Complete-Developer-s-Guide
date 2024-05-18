ed3d092b0c0fe5b9a837c572393bd468e45c239a
Michael McLean <g4m3rm1k3@hotmail.com>
2024-05-18 16:35:44 -0400

    first commit

---
+++ features/annotations/variables.ts
@@ -0,0 +1,49 @@
+let apples: number = 5;
+
+apples = 15;
+let speed: string = 'fast';
+let hasName: boolean = true;
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+// built in objects
+let now: Date = new Date();
+
+
+//Array
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1,2,3,4,5];
+let truths: boolean[] = [true, true, false];
+
+// Classes
+class Car{};
+
+let car: Car = new Car();
+
+//object literal
+let point: { x: number; y: number } = {
+	x: 10,
+	y: 20,
+};
+
+// Function
+const logNumber: (i:number) => void = (i:number) => {
+  console.log(i);
+};
+
+// when to use annotations
+// 1) Function that returns the 'any' type
+const json = '{"x":10,"y":20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+
+console.log(coordinates); // {x:10, y:20};
+
+// 2) When we declare a variable on one line
+// and initalize it later
+let words = ['red', 'green','blue'];
+let foundWord;
+
+for (let i = 0; i < words.length; i++){
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
